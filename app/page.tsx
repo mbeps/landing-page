29a501c9d3a7acc7f49cdc9d552895fd6962c976
Maruf Bepary <bepary71@gmail.com>
2023-05-14 14:12:49 +0100

    Added featured section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import Features from "@/components/Features"
 import HeroSection from "@/components/HeroSection"
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Features />
     </>
   )
 }
