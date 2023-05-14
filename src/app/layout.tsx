70b53fe3fc4b9e671c69263b861023c92de51f99
Maruf Bepary <bepary71@gmail.com>
2023-05-14 16:27:20 +0100

    Centred content in the main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+import { ChakraBaseProvider, Flex, extendBaseTheme } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
@@ -12,9 +12,21 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ChakraBaseProvider>
-        <Navbar />
-          {children}
-        <Footer />
+          {/*  */}
+          <Navbar />
+          <Flex
+            justify="center"
+            content="center"
+            p="16px 0px"
+            direction="column"
+            justifyContent="center"
+            border="1px solid red"
+            alignItems="center" // Add this line to center the content vertically
+          >
+            {children}
+          </Flex>
+
+          <Footer />
         </ChakraBaseProvider>
       </body>
     </html>
